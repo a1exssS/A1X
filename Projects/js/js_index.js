0679743451e4405b0decc4db0d79d96a64e8e52f
a1exss <porkkoa@gmail.com>
2022-07-13 09:59:09 +0300

    A1X changes

--- Projects/js/js_index.js
+++ Projects/js/js_index.js
@@ -3,9 +3,9 @@
 document.getElementById('headerSearchBtn').onclick = function () {
 	let s1mple = document.getElementById('headerSearch').value;
 	if (s1mple == "s1mple") {
-		setTimeout(function () {
-			window.location.href = 'https://www.youtube.com/';
-		});
+		// setTimeout(function () {
+		window.location.href = 'https://www.youtube.com/';
+		// });
 	}
 	else {
 		alert(`Sorry, but we didn't ${s1mple}`)
