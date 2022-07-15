bf753a1d752397648b7d4bd40cb277e154e843ae
a1exss <porkkoa@gmail.com>
2022-07-15 12:23:47 +0300

    header is finished A1X

--- Projects/js/js_index.js
+++ Projects/js/js_index.js
@@ -1,5 +1,5 @@
 "Use strict"
-
+// search bar
 document.getElementById('headerSearchBtn').onclick = function () {
 	let s1mple = document.getElementById('headerSearch').value;
 	if (s1mple == "s1mple") {
@@ -11,3 +11,26 @@ document.getElementById('headerSearchBtn').onclick = function () {
 		alert(`Sorry, but we didn't ${s1mple}`)
 	}
 }
+
+// clock
+setInterval(() => {
+	let hours = document.getElementById('hours')
+	let minutes = document.getElementById('minutes')
+	let ampm = document.getElementById('ampm')
+
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+
+	let am = h >= 12 ? "PM" : "AM"
+
+	if (h > 12) {
+		h = h - 12
+	}
+
+	h = (h < 10) ? "0" + h : h
+	m = (m < 10) ? "0" + m : m
+
+	hours.innerHTML = h;
+	minutes.innerHTML = m;
+	ampm.innerHTML = am;
+})
