3fb0ac20ca7d8949ac803706703ecbb18a0a6f66
a1exss <porkkoa@gmail.com>
2022-07-25 12:27:06 +0300

    new changes (again)

--- Projects/js/CodeWars_js.js
+++ Projects/js/CodeWars_js.js
@@ -217,6 +217,7 @@ console.log(minMax([2342, 4574, 46, 2]))*/
 
 
 
+
 // function repeatStr(n, s) {
 // 	let i = 1
 // 	let a = s
@@ -227,14 +228,157 @@ console.log(minMax([2342, 4574, 46, 2]))*/
 // }
 // console.log(repeatStr(3, 's'))
 
-function invert(array) {
-	let i = 0;
-	let length = array.length
-	let a = []
-	for (i, length; i < length; ++i) {
-		a.push(array[i] * -1)
-	}
-	return a
-}
-console.log(invert([234, 6, 5, 6, 6, -5,]))
 
+
+
+// function invert(array) {
+// 	let i = 0;
+// 	let length = array.length
+// 	let a = []
+// 	for (i, length; i < length; ++i) {
+// 		a.push(array[i] * -1)
+// 	}
+// 	return a
+// }
+// console.log(invert([234, 6, 5, 6, 6, -5,]))
+
+
+
+
+// function descendingOrder(n) {
+// 	let array = []
+// 	array.push(n)
+// 	array = String(array)
+// 	array = array.split('')
+// 	array.sort((a, b) => b - a)
+// 	array = array.join("")
+// 	return array++
+// }
+// console.log(descendingOrder(345))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// map and filters statements
+
+const items = [
+	{ name: 'a1exss', age: 16 },
+	{ name: 'Qman24', age: 17 },
+	{ name: 'wayM', age: 13 },
+	{ name: 'Deffo', age: 17 },
+	{ name: 'ReNN', age: 16 },
+	{ name: 'Kama', age: 18 },
+]
+
+// fillter
+const filterAges = items.filter((age) => {
+	return age.age >= 16 && age.name == 'a1exss'
+	// return age.age >= 14
+})
+// map
+const mapNames = items.map((name) => {
+	return name.age
+})
+// find
+const findItems = items.find((item) => { return item.name === 'Qman24' })
+
+// forEach
+const forEachItems = items.forEach((item) => {
+	console.log(item.age)
+})
+
+// some
+const someAge = items.some((item) => {
+	return item.age > 12
+})
+
+// every
+const everyItem = items.every((item) => {
+	return item.age <= 18
+})
+
+// reduce
+const reduceAges = items.reduce((sum, item) => {
+	return item.age + sum
+}, 0)
+
+// includes
+const includesItems = mapNames.includes(16)
+
+console.log(filterAges);
+console.log(mapNames);
+console.log(findItems);
+console.log(someAge);
+console.log(everyItem);
+console.log(reduceAges);
+console.log(includesItems);
+
+
+// match method in JS
+
+// whithout limit after d{3}
+
+let matches = /\d{3}/
+console.log(matches.test('323abasdfadfac'))
+
+// whith limit after /^\d{3}$/
+
+let newMatches = /^\d{4}$/
+console.log(newMatches.test('1111'))
+
+// w+ = word plus something; \ = after that (there are sould be any values)
+
+let text = /\w+@\w+\.(net|org|com)/
+console.log(text.test('a@a.com'))
+
+// match can return that worn in array but if it didn't found that word in return null
+let newText = 'hello a1exss!'
+let randomName = /a1exss/
+console.log(newText.match(randomName))
+console.log(randomName.test('a1exss'))
+
+// [] it takes all things that you wrote in it
+let anotherText = 'hello guys, my name is a1exss!'
+let randomName2 = /[a-z1]+/g;
+console.log(anotherText.match(randomName2))
+
+// \b[a-z1]+\b it tells to don't take in array that things that didn't contain something
+let anotherText2 = 'Hello Guys, my name is a1exss!'
+let randomName3 = /\b[a-z1]+\b/g;
+console.log(anotherText2.match(randomName3))
+
+// /gi takes all stuff that you wrote
+let anotherText3 = 'Hello Guys, My Name Is A1exss!'
+let randomName4 = /[a-z1,]+/gi;
+console.log(anotherText3.match(randomName4))
+
+//  no comments :)
+let myNumber = '9825-346-23456-26'
+// let randomName5 = /\d+-\d+/gi;
+let randomName5 = /(\d{3})[-]\d{3}/gi;
+console.log(myNumber.match(randomName5))
+*/
